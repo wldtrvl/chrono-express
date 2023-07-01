bab5cbb8339e207ca6a8deb6eb78588c343696cb
Ilya Marchenko <work.marchenko.i@gmail.com>
2023-07-01 19:23:12 +0300

    build(setup): Extract App as class, add nodemon and env configs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,36 @@
 import express from "express";
-const app = express();
-const port = 3000;
+import cors from "cors";
+import { NODE_ENV, PORT } from "@config";
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+export class App {
+    public app: express.Application;
+    public env: string;
+    public port: string | number;
+
+    constructor() {
+        this.app = express();
+        this.env = NODE_ENV || "development";
+        this.port = PORT || 3000;
+
+        this.initializeMiddlewares();
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`=================================`);
+            console.log(`======= ENV: ${this.env} =======`);
+            console.log(`App listening on the port ${this.port}`);
+            console.log(`=================================`);
+        });
+    }
+
+    public getServer() {
+        return this.app;
+    }
+
+    private initializeMiddlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+    }
+}
